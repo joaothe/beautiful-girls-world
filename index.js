958bc9814731b7d37e0d840b72d8937f309f1e8b
Joao Moraes <joao.moraes@cwi.com.br>
2020-01-29 00:11:53 -0300

    Sending project

---
+++ index.js
@@ -0,0 +1,21 @@
+const request = require('request'),
+	cheerio = require('cheerio'),
+	fs = require('fs');
+
+let images = [];
+request('https://vogue.globo.com/moda/gente/noticia/2016/08/segundo-estudo-cientifico-estas-sao-10-mulheres-mais-lindas-do-mundo.html', function(err, res, body){
+	if(!err && res.statusCode == 200){
+        //console.log(res)
+        //console.log(cheerio.load(body))
+		const $ = cheerio.load(body);
+		$('image', 'div.protected-content').each(function(){
+		const img = $(this).attr('data-src');
+		images.push('http://'+img.slice(2, img.length));
+
+		})
+		console.log(images);
+		for (var i = 0; i < images.length; i++){
+			request(images[i]).pipe(fs.createWriteStream('images/beautiful_girls' + i + '.jpg'));
+		}
+	}
+});
